f736ccce8b12e2c51a83edab2ac041e0e65bb58f
Shashank Karan <shashankkaran999@gmail.com>
2021-12-08 08:49:00 +0530

    done form

---
+++ src/pages/becomemodel/GoogleForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GoogleForm() {
+    return (
+        <div class="center pb-5">
+            <a href="https://docs.google.com/forms/d/1ElKeMTs8CslLsMR3IBBqYEbrNZNJ5hH9YrGCFwXCU7Q/edit">
+                <svg fill="#FCD34D" class="center mb-2 ml-auto mr-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 2v20h-20v-20h20zm2-2h-24v24h24v-24zm-4 7h-8v1h8v-1zm0 5h-8v1h8v-1zm0 5h-8v1h8v-1zm-10.516-11.304l-.71-.696-2.553 2.607-1.539-1.452-.698.71 2.25 2.135 3.25-3.304zm0 5l-.71-.696-2.552 2.607-1.539-1.452-.698.709 2.249 2.136 3.25-3.304zm0 5l-.71-.696-2.552 2.607-1.539-1.452-.698.709 2.249 2.136 3.25-3.304z"/></svg>
+             Link to official Google Form
+            </a>
+        </div>
+    )
+}
+
+export default GoogleForm
